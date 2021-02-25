dbf5c6f10b5efebf3c2e192ce8a8df5923271e15
chenguang.zhuang <1>
2021-02-25 13:48:19 +0800

    zcg

--- py_01day/book.py
+++ py_01day/book.py
@@ -1,7 +1,31 @@
 import pickle
+import os
 
 
-class BookManage(object):
+class Book:
+    def __init__(self, num, name, position):
+        """
+        图书属性
+        :param num: 图书编号
+        :param name: 图书名字
+        :param position: 图书位置
+        :return:
+        """
+        self.num = num
+        self.name = name
+        self.position = position
+
+    def __str__(self):
+        """
+        "自定义print对象时显示的格式
+        :return:
+        """
+        return f"{self.num}\t{self.name}\t{self.position}"
+
+
+class BookManage:
+    # 定义一个图书库
+    book_lists = []
 
     def __init__(self):
         """
@@ -9,11 +33,98 @@ class BookManage(object):
         """
         print("=====__int__方法正在执行!=========")
 
-        book_lists = pickle.load(open("book.data"), "rb")
+        # 避免异常有两种方法：一、加判断；二、异常处理
+
+        # 确保每次运行程序前，先把book.data文件中所有内存load到book_list中
+        if os.path.isfile("book.data"):
+            self.book_lists = pickle.load(open("book.data", "rb"))  # 将文件数据加载到内存中
+            print(self.book_lists)
+        else:
+            pickle.dump(self.book_lists, open("book.data", "wb"))  # 将内存数据保存在文件中
+
+        # try:
+        #     self.book_lists = pickle.load(open("book.data", "rb"))  # 将文件数据加载到内存中
+        # except FileNotFoundError as e:
+        #     pickle.dump(self.book_lists, open("book.data", "wb"))  # 将内存数据保存在文件中
+        #     print("文件创建成功！")
+
+    def show_all_books(self):
+        """
+        展示所有的图书
+        :return:
+        """
+        for book in self.book_lists:
+            print(book)
+
+    def add_book(self, book: Book):
+        """
+        添加图书
+        :return:
+        """
+        self.book_lists.append(book)
+
+    def del_book(self, num):
+        """
+        删除图书
+        :return:
+        """
+        num = int(num)
+        for book in self.book_lists:
+            if num == book.num:
+                self.book_lists.remove(book)
+                print("删除图书成功！")
+                break
+        else:
+            print("图书编号{}不存在".format(num))
+
+    def find_book(self, num):
+        """
+        查看图书信息
+        :param num:
+        :return:
+        """
+        num = int(num)
+        for book in self.book_lists:
+            if num == book.num:
+                print(book)
+                print("查找图书成功！")
+                break
+        else:
+            print("图书编号{}不存在".format(num))
+
+    def clear(self):
+        """
+        清空数据
+        :return:
+        """
+        self.book_lists.clear()
+        # self.book_lists = []
+        print("数据已清空！")
+
+    def save(self):
+        """
+        保存数据
+        :return:
+        """
+        pickle.dump(self.book_lists, open("book.data", "wb"))
+        print("图书保存成功！")
+
+    @property
+    def last_book_id(self):
+        """
+        如果book_list是空，返回0，否则,返回最后一本书的num值
+        :return:
+        """
+        if self.book_lists:
+            book = self.book_lists[-1]  # 获取列表最后一本书
+            return book.num
+        return 0
 
     def __del__(self):
         """
         析构函数
         :return:
         """
-        pickle.dump()
+        print("__del__ is running ....")
+        print(self.book_lists)
+        pickle.dump(self.book_lists, open("book.data", "wb"))
--- py_01day/main.py
+++ py_01day/main.py
@@ -1,4 +1,4 @@
-from book import BookManage
+from py_01day.book import BookManage, Book
 
 
 def welcome():
@@ -6,20 +6,52 @@ def welcome():
     欢迎提示语：
     :return:
     """
-    print("=====欢迎来到图书管理系统======")
-    print("请选择你要做的操作：\n1、显示所有图书\n2、添加图书\n3、删除\n4、退出")
-    print("=====欢迎来到图书管理系统======")
+    print("*******************欢迎进入图书管理系统**********************")
+    print("1.显示所有图书\n2.添加图书\n3.删除图书\n4.查找图书\n5.退出\n6.清空数据\n7.保存")
+    print("**********************************************************")
+
+
+def choose_num():
+    """
+    选择数字
+    :return: 返回选择的数字
+    """
+    num = input("请输入你的选择：")
+    # 如果编号不是数字  或者  不是 1，2,3,4,5  返回 -1
+    if not num.isdigit() or num not in ["1", "2", "3", "4", "5", "6", "7"]:
+        return "-1"
+    return num
 
 
 def main():
-    # 创建一个对象
-    bm = BookManage()
-    welcome()
+    bm = BookManage()  # 创建一个图书管理系统的对象
     while True:
-        choose = input("请输入你的选择：")
-        if choose == "1":
-            show_all_books()
-
+        welcome()  # 欢迎提示语
+        choose = choose_num()  # 选择数字
+        if choose == "-1":
+            print("输入的有误，请重新输入！")
+        elif choose == "1":
+            bm.show_all_books()
+        elif choose == "2":
+            num = bm.last_book_id + 1
+            name = input("请输入图书名字：")
+            position = input("请输入图书位置：")
+            book = Book(num, name, position)
+            bm.add_book(book)
+        elif choose == "3":
+            num = input("请输入要删除的图书编号：")
+            bm.del_book(num)
+        elif choose == "4":
+            num = input("请输入要查找的图书编号：")
+            bm.find_book(num)
+        elif choose == "5":
+            break
+        elif choose == "6":
+            bm.clear()
+        elif choose == "7":
+            bm.save()
+        else:
+            break
 
 
 if __name__ == '__main__':
