7bea5aec0a0c85395daa843eebf33c145b771a93
chenguang.zhuang <1>
2021-03-03 19:32:29 +0800

    zcg

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
+import time
 
-num = "1"
 
-print(num.isdigit())
+def get_time_stamp():
+    ct = time.time()
+    local_time = time.localtime(ct)
+    data_head = time.strftime("%Y-%m-%d %H-%M-%S", local_time)
+    data_secs = (ct - int(ct)) * 1000
+    time_stamp = "%s-%03d" % (data_head, data_secs)
+    return time_stamp
+
+
+num = get_time_stamp()
+
+print(num)
