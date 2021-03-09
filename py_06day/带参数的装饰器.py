744953d218f03c09e87968af74e0653bd3ddb6b8
chenguang.zhuang <1>
2021-03-09 20:57:43 +0800

    zcg

--- py_06day/带参数的装饰器.py
+++ py_06day/带参数的装饰器.py
@@ -30,11 +30,17 @@ def add(a, b):
 
 
 class Log1:
+    print("执行Log1类")
+
     def __init__(self, filename: str):
         self.filename = filename
+        print("执行__init__函数")
 
     def __call__(self, func):
+        print("执行__call__函数")
+
         def wrapper(*args, **kwargs):
+            print("执行wrapper函数")
             print(args)
             print(self.filename)
             results = func(*args, **kwargs)
@@ -44,6 +50,7 @@ class Log1:
 
 @Log1(filename="xxx.log")
 def add1(a, b):
+    print("执行add1函数")
     return a + b
 
 
